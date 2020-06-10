4ead48e467b4bd1d0199751f624ccee32c8d4363
zmalakhov <zm.13.15.zm@gmail.com>
2020-06-10 13:22:46 +0300

    #2 - Навбар - Навбар и меню

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
     <div class="wrapper">
         <header>
-            <h1>Header</h1>
+            <div class="navbar">
+                <div class="container">
+                    <div class="navbar-content">
+                        <div class="logo">VUE-CLI</div>
+                        <ul class="navbar-list">
+                            <li class="navbar-item">
+                                <a class="navbar-link">Home</a>
+                            </li>
+                            <li class="navbar-item">
+                                <a class="navbar-link">Example</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </header>
 
         <router-view></router-view>
