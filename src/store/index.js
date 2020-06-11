ab0262af3c4a7a7ce782ac28903222e2c4c1208d
zmalakhov <zm.13.15.zm@gmail.com>
2020-06-11 09:39:18 +0300

    #8.3 - Базис - Geters/state и тд

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,22 @@ import Vuex from 'vuex';
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
-    getters: {},
+    state: {
+        message: 'hello vuex'
+    },
+    mutations: {
+        setMessage (state) {
+            state.message = 'hello again'
+        }
+    },
+    actions: {
+        setMessage ({commit}){
+            commit('setMessage')
+        }
+    },
+    getters: {
+        getMessage(state) {
+            return state.message
+        }
+    },
 })
